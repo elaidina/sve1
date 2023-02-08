b220788385bc7ed8c2dfa2d06705774e7a17ce5f
Your Name <you@example.com>
2023-02-08 16:47:02 +0100

    l32

--- level25.js
+++ level25.js
@@ -181,7 +181,7 @@ audio1.play();
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
     if  (cardsWon.length === cardArray.length/2) {
-      resultDisplay.innerHTML = " <h1>Congratulations! You found them all!</h1><h2>Level 22 completed!</h2><a href='https://elaidina.github.io/sve1/leve23.html'> Continue to Level 23</a>";
+      resultDisplay.innerHTML = " <h1>Congratulations! You found them all!</h1><h2>Level 25 completed!</h2><a href='https://elaidina.github.io/sve1/leve26.html'> Continue to Level 26</a>";
 
 
       var audio3 = new Audio ("images/end.mp3")
--- level31.js
+++ level31.js
@@ -181,7 +181,7 @@ audio1.play();
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
     if  (cardsWon.length === cardArray.length/2) {
-      resultDisplay.innerHTML = " <h1>Congratulations! You found them all!</h1><h2>Level 22 completed!</h2><a href='https://elaidina.github.io/sve1/leve23.html'> Continue to Level 23</a>";
+      resultDisplay.innerHTML = " <h1>Congratulations! You found them all!</h1><h2>Level 31 completed!</h2><a href='https://elaidina.github.io/sve1/leve32.html'> Continue to Level 32</a>";
 
 
       var audio3 = new Audio ("images/end.mp3")
